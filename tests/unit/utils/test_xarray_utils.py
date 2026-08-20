2a9c344216fe811dfd2ee853a82f3d6eb4580ce2
Thomasjkeel <thomasjames.keel@googlemail.com>
2026-08-20 16:02:42 +0100

    add tests

---
+++ tests/unit/utils/test_xarray_utils.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""Tests for batch saving utility functions."""
+
+import numpy as np
+import polars as pl
+import pytest
+
+from rainfall_gridder.utils import xarray_utils
+
+
+
