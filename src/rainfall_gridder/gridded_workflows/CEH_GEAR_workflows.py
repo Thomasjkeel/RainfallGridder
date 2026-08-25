d2f4141ef483e8dd08f56cffe723573b9449d68c
Thomasjkeel <thomasjames.keel@googlemail.com>
2026-08-25 13:15:36 +0100

    clean up write to zarr logic

--- src/rainfall_gridder/gridded_workflows/CEH_GEAR_workflows.py
+++ src/rainfall_gridder/gridded_workflows/CEH_GEAR_workflows.py
@@ -342,25 +342,28 @@ def produce_sub_daily_ceh_gear(config, gridded_rainfall, qcd_rainfall_data, corr
             sub_daily_ceh_gear_batch.append(ceh_gear_sub_daily_one_day)
 
         if valid_time_steps_processed > 0:
-            if any_batches_processed:
-                allow_overwrite = False
-            print(allow_overwrite, valid_time_steps_processed)
-            write_to_zarr(config, allow_overwrite, sub_daily_ceh_gear_batch)
+            print(allow_overwrite, any_batches_processed, valid_time_steps_processed)
+            write_to_zarr(config, allow_overwrite, sub_daily_ceh_gear_batch, any_batches_processed)
             any_batches_processed = True
 
 
-def write_to_zarr(config, allow_overwrite, sub_daily_ceh_gear_batch):
+def write_to_zarr(config, allow_overwrite, sub_daily_ceh_gear_batch, any_batches_processed):
     if not sub_daily_ceh_gear_batch:
         return
     zarr_output_file_exists = os.path.exists(config.output_dir / config.output_zarr_name)
-    if zarr_output_file_exists and not allow_overwrite:
-        raise ValueError(f"Zarr output file already exists: {config.output_dir / config.output_zarr_name}. If you'd like to overwrite, set allow_zarr_overwrite=True")
-
-    combined_batch_ds = xr.concat(sub_daily_ceh_gear_batch, dim="time", join="outer")
+    if zarr_output_file_exists and not any_batches_processed:
+        if not allow_overwrite:
+            raise ValueError(f"Zarr output file already exists: {config.output_dir / config.output_zarr_name}. If you'd like to overwrite, set allow_zarr_overwrite=True")
+        mode = "w"
+    elif zarr_output_file_exists:
+        mode = "a"
+    else:
+        mode = "w"
+    combined_batch_ds = xr.concat(sub_daily_ceh_gear_batch, dim="time", join="exact")
     combined_batch_ds = combined_batch_ds.chunk("auto")
     del sub_daily_ceh_gear_batch
 
-    if zarr_output_file_exists and not allow_overwrite:
+    if mode == "a":
         combined_batch_ds.to_zarr(
             config.output_dir / config.output_zarr_name, align_chunks=True, append_dim="time", zarr_format=2
         )
