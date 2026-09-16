916f7ed79b5b6e778154dcf1c37488e0063c1f6d
Thomasjkeel <thomasjames.keel@googlemail.com>
2026-09-16 14:32:48 +0100

    add subhourly_qc

--- src/rainfall_gridder/quality_control/intenseQC_framework.py
+++ src/rainfall_gridder/quality_control/intenseQC_framework.py
@@ -37,3 +37,42 @@ intenseqc_framework = {
         "function": neighbourhood_checks.check_monthly_neighbours,
     },
 }
+
+INTENSEQC_W_SUBHOURLYQC_RULEBASE = {
+    "QC2": {
+        "function": gauge_checks.check_years_where_annual_kth_largest_value_is_zero,
+    },
+    "QC10": {
+        "function": comparison_checks.check_exceedance_of_rainfall_world_record,
+    },
+    "QC11": {
+        "function": comparison_checks.check_hourly_exceedance_etccdi_rx1day,
+    },
+    "QC12": {
+        "function": timeseries_checks.check_dry_period_cdd,
+    },
+    "QC13": {
+        "function": timeseries_checks.check_daily_accumulations,
+    },
+    "QC14": {
+        "function": timeseries_checks.check_monthly_accumulations,
+    },
+    "QC15": {
+        "function": timeseries_checks.check_streaks,
+    },
+    "QC17": {
+        "function": neighbourhood_checks.check_wet_neighbours_hourly,
+    },
+    "QC19": {
+        "function": neighbourhood_checks.check_dry_neighbours_hourly,
+    },
+    "QC20": {
+        "function": neighbourhood_checks.check_monthly_neighbours,
+    },
+    "HQC_UK1hr": {"function": subhourlyqc_checks.check_exceedance_of_UK_1hr_record},
+    "HQC_UK24hr": {"function": subhourlyqc_checks.check_exceedance_of_UK_24hr_record},
+    "HQC_UK24hr_rolling": {"function": subhourlyqc_checks.check_daily_exceedance_of_UK_24hr_record},
+    "HQC_streaks_20mm": {"function": subhourlyqc_checks.check_streaks_20mm},
+    "SHQC_freqResChecker": {"function": subhourlyqc_checks.check_freq_is_subhourly},
+    "SHQC_subH_checkr": {"function": subhourlyqc_checks.check_subhourly_thresholds},
+}
