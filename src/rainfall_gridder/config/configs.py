7dc3b3342d18c0e84b6c2111caffab7d5808f0db
Thomasjkeel <thomasjames.keel@googlemail.com>
2026-09-16 14:30:53 +0100

    update qc_framework

--- src/rainfall_gridder/config/configs.py
+++ src/rainfall_gridder/config/configs.py
@@ -12,7 +12,7 @@ def get_ceh_gear_15m_CEH_GEAR_based_kwargs():
         "time_res": "15m",
         "min_n_neighbours": 7,
         "n_closest_neighbours": 10,
-        "qc_framework": "intenseqc_rulebase_only",
+        "qc_framework": "intenseqc_w_subhourlyqc_rulebase",
         "nearby_rainfall_data_loader_kwargs": {
             "distance_threshold_km": 50,
             "n_closest_neighbours": 10,  # TODO: how many neighbours are needed for QC check
@@ -30,7 +30,7 @@ def get_ceh_gear_15m_HadUK_Grid_based_kwargs():
         "min_n_timesteps": 96,  # TODO: change
         "time_res": "15m",
         "n_closest_neighbours": 10,
-        "qc_framework": "intenseqc_rulebase_only",
+        "qc_framework": "intenseqc_w_subhourlyqc_rulebase",
         "nearby_rainfall_data_loader_kwargs": {
             "distance_threshold_km": 50,
             "n_closest_neighbours": 10,  # TODO: how many neighbours are needed for QC check
